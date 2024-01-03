37cf45c18e25300364b6ea8afc65671fb7d26420
Achitei Marius <marius.achitei02@gmail.com>
2024-01-03 23:07:38 +0200

    memory graph per bytes and support from line_chart class

--- tabs/common/line_chart.py
+++ tabs/common/line_chart.py
@@ -1,40 +1,66 @@
+import customtkinter
 import matplotlib.pyplot as plt
 import mplcyberpunk
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+from utils.general_utils import choose_equidistant_timestamps, seconds2human
+
+NUMBER_OF_TIMESTAMPS = 8
+
 
 class LineChart:
-    def __init__(self, root, update_function, effects):
+    def __init__(self, root, update_function, effects, y_limit=(0, 100), y_label_function=lambda x: x):
         self.root = root
         self.update_function = update_function
         self.effects = effects
         self.usage = []
+        self.y_label_function = y_label_function
+        self.y_limit = y_limit
         self.fig, self.ax = plt.subplots()
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas.get_tk_widget().pack()
 
-        self.bar = ttk.Progressbar(root, style="modern.Horizontal.TProgressbar", orient="horizontal",
-                                   length=200, mode="determinate")
+        self.bar = customtkinter.CTkProgressBar(root, width=200, height=20, corner_radius=0)
+        # if y_limit[0] == 0 and y_limit[1] == 100:
         self.bar.pack(pady=30)
 
-        self.label = tk.Label(root, text='0%', bg='white')
+        # self.label = customtkinter.CTkLabel(root, text='0%', bg='white')
+        self.label = customtkinter.CTkLabel(root, text='0%', width=20, anchor="w")
         self.label.place(in_=self.bar, relx=1.0, x=20, rely=0)  # Adjust the position as neede
         self.update_line_chart()
 
     def update_line_chart(self):
         value = self.update_function()
-        self.bar['value'] = value
+        self.bar.set(value / self.y_limit[1])
         self.usage.append(value)
         self.ax.clear()
-        self.ax.set_ylim(0, 100)
+        self.ax.set_ylim(self.y_limit[0], self.y_limit[1])
         self.ax.plot(self.usage)
+        self.ax.set_ylabel('Usage')
+        self.ax.set_xlabel('Time')
+
+        labels = [label.get_position()[1] for label in self.ax.get_yticklabels()]
+        y_labels = list(map(self.y_label_function, labels))
+
+        self.ax.set_yticks(self.ax.get_yticks())
+        self.ax.set_yticklabels(y_labels)
+
+        timestamps = choose_equidistant_timestamps(len(self.usage), NUMBER_OF_TIMESTAMPS)
+        x_ticks = [i for i, timestamp in enumerate(self.usage) if timestamp in timestamps]
+        x_labels = [seconds2human(timestamp) for timestamp in timestamps]
+
+        self.ax.set_xticks(timestamps)
+        self.ax.set_xticklabels(x_labels)
+
         self.effects(self.ax)
-        self.ax.set_title("CPU Usage")
+        self.ax.set_title("Usage")
         self.canvas.draw()
 
-        self.label['text'] = f'{value}%'
+        if self.y_limit[0] == 0 and self.y_limit[1] == 100:
+            self.label.configure(text=f"{value}%")
+        else:
+            self.label.configure(text=f"{self.y_label_function(value)} / {self.y_label_function(self.y_limit[1])}")
 
-        # Actualizează graficul la fiecare secundă
         self.root.after(1000, self.update_line_chart)
--- tabs/memory.py
+++ tabs/memory.py
@@ -7,6 +7,8 @@ from tabs.common.line_chart import LineChart
 import matplotlib.pyplot as plt
 import psutil
 import mplcyberpunk
+from utils.memory_utils import get_memory_type_by_code
+from utils.general_utils import bytes2human
 
 WIDTH = 500
 HEIGHT = 400
@@ -17,10 +19,26 @@ def init_memory(root):
     MemoryTab(root)
 
 
-def memory_update_function():
+def memory_precent_update_function():
     return psutil.virtual_memory().percent
 
 
+def memory_bytes_update_function():
+    return psutil.virtual_memory().used
+
+
+def get_memory_limit():
+    return 0, psutil.virtual_memory().total
+
+
+def y_label_function(x):
+    if type(x) == int:
+        return bytes2human(int(x))
+    return bytes2human(int(float(x)))
+
+
+# 0740688443
+# 0765063494
 def get_ram_info():
     cmd = "wmic MemoryChip get /format:csv"
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -36,6 +54,10 @@ def get_ram_info():
         values = [value for value in line.split(",")]
         for i in range(0, len(values)):
             slot[keys[i]] = values[i]
+        slot["Capacity"] = bytes2human(int(slot["Capacity"]))
+        slot["ConfiguredClockSpeed"] = f"{slot['ConfiguredClockSpeed']} MHz"
+        slot["Speed"] = f"{slot['Speed']} MHz"
+        slot["MemoryType"] = get_memory_type_by_code(slot["MemoryType"])
         data.append(slot)
     if error:
         return error
@@ -51,7 +73,8 @@ class MemoryTab:
         self.tabview.tab("info").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
         self.tabview.tab("usage").grid_columnconfigure(0, weight=1)
         self.populate_info()
-        LineChart(self.tabview.tab("usage"), memory_update_function, mplcyberpunk.add_glow_effects)
+        self.populate_usage()
+        # LineChart(self.tabview.tab("usage"), memory_update_function, mplcyberpunk.add_glow_effects)
 
     def populate_info(self):
         self.infotabview = customtkinter.CTkTabview(self.tabview.tab("info"), width=WIDTH - 20, height=HEIGHT - 20)
@@ -66,3 +89,18 @@ class MemoryTab:
                                            height=HEIGHT - 20)
             info_tab.grid(row=0, column=0, padx=15, pady=15, sticky="ns")
             self.info_tabs.append(info_tab)
+
+    def populate_usage(self):
+        self.usagetabview = customtkinter.CTkTabview(self.tabview.tab("usage"), width=WIDTH, height=HEIGHT)
+        self.usagetabview.grid(row=0, column=2, sticky="nsew")
+        self.usagetabview.add("precent")
+        self.usagetabview.add("bytes")
+        self.usagetabview.tab("precent").grid_columnconfigure(0, weight=1)
+        self.usagetabview.tab("bytes").grid_columnconfigure(0, weight=1)
+        self.usage_tabs = []
+        precent_usage_tab = LineChart(self.usagetabview.tab("precent"), memory_precent_update_function,
+                                      mplcyberpunk.add_glow_effects)
+        bytes_usage_tab = LineChart(self.usagetabview.tab("bytes"), memory_bytes_update_function,
+                                    mplcyberpunk.add_glow_effects,
+                                    y_limit=get_memory_limit(),
+                                    y_label_function=y_label_function)
